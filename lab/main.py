8dca3e2189e5bb4da8382a3137730d1c680b0d34
@dmytro.bobyk <dmytro.bobyk@pettersonapps.com>
2023-02-04 12:06:52 +0200

    function for darwing plots

--- lab/main.py
+++ lab/main.py
@@ -1,6 +1,22 @@
+from typing import Tuple
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def draw_plot(xdata, ydata, xlabel: str, ylabel: str, title: str) -> Tuple[plt.Figure, plt.Axes]:
+    fig, ax = plt.subplots()
+    ax.plot(xdata, ydata)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+    plt.grid(linestyle='--', color="black", alpha=0.4)
+
+    plt.show()
+
+    return fig, ax
+
 
 if __name__ == "__main__":
     try:
@@ -12,7 +28,8 @@ if __name__ == "__main__":
 
         f0 = 1 / T
 
-        testk_file_path = f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\\test{VARIANT}.txt'
+        testk_file_path = \
+            f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\\test{VARIANT}.txt'
 
         # read testk.txt
         with open(testk_file_path, 'r', encoding='utf-8') as file:
@@ -29,12 +46,9 @@ if __name__ == "__main__":
 
         t = np.arange(0, T + d, d)  # np.arange doesn't include stop value
 
-        # plot values from textk.txt
-        # fig, ax = plt.subplots()
-        # ax.plot(t, x_i)
-        # ax.set_xlabel('t')
-        # ax.set_ylabel('x')
-        # plt.show()
+        # plot of values from textk.txt
+        draw_plot(xdata=t, ydata=x_i, xlabel='t', ylabel='x',
+                  title=f'Plot of values from text{VARIANT}.txt')
 
         cx_k = []
         for k in range(N):
@@ -47,19 +61,14 @@ if __name__ == "__main__":
             cx_k.append((real_sum + imaginary_sum * 1j) / N)
         cx_k_abs = [abs(cx) for cx in cx_k]
 
-        # plot values of cx_k_abs with coresponding N
-        # fig, ax = plt.subplots()
-        # ax.plot(np.arange(0, N, 1), cx_k_abs)
-        # ax.set_xlabel('N')
-        # ax.set_ylabel('cx_k_abs')
-        # plt.show()
+        # plot values of cx_k_abs with corresponding k
+        draw_plot(xdata=np.arange(0, N, 1), ydata=cx_k_abs, xlabel='k',
+                  ylabel='cx_k_abs', title='Plot of cx_k absolute values corresponding to k')
 
-        # plot first half of values of cx_k_abs with coresponding frequency
-        fig, ax = plt.subplots()
-        ax.plot(np.arange(0,  (N // 2 + 1) * f0, f0), cx_k_abs[:N // 2 + 1])
-        ax.set_xlabel('N')
-        ax.set_ylabel('cx_k_abs')
-        plt.show()
+        # plot first half of values of cx_k_abs with corresponding frequency
+        draw_plot(xdata=np.arange(0,  (N // 2 + 1) * f0, f0),
+                  ydata=cx_k_abs[:N // 2 + 1], xlabel='frequency', ylabel='cx_k_abs',
+                  title='Plot of cx_k absolute values corresponding to frequency')
 
     except Exception as e:
         print('Error occured:', e, sep='\n')
